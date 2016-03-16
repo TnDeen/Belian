657d69f08fef35e072b90e21c3de1a9fc18abcbb
Salehudin <salehuddin@mail.puncaktegap.com.my>
2016-03-16 13:15:01 +0800

    add export function

--- src/main/java/com/example/mohdsalehudin/testapp/Belian.java
+++ src/main/java/com/example/mohdsalehudin/testapp/Belian.java
@@ -23,6 +23,8 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.opencsv.CSVWriter;
+
 import java.io.File;
 import java.io.FileWriter;
 import java.util.List;
@@ -99,6 +101,36 @@ public class Belian extends ListActivity {
     }
 
     private void ExportDB(){
+
+        File dbFile=getDatabasePath("commments.db");
+        MySQLiteHelper dbhelper = new MySQLiteHelper(getApplicationContext());
+        File exportDir = new File(Environment.getExternalStorageDirectory(), "");
+        if (!exportDir.exists())
+        {
+            exportDir.mkdirs();
+        }
+
+        File file = new File(exportDir, "csvname.csv");
+        try
+        {
+            file.createNewFile();
+            CSVWriter csvWrite = new CSVWriter(new FileWriter(file));
+            SQLiteDatabase db = dbhelper.getReadableDatabase();
+            Cursor curCSV = db.rawQuery("SELECT * FROM comments",null);
+            csvWrite.writeNext(curCSV.getColumnNames());
+            while(curCSV.moveToNext())
+            {
+                //Which column you want to exprort
+                String arrStr[] ={curCSV.getString(0),curCSV.getString(1)};
+                csvWrite.writeNext(arrStr);
+            }
+            csvWrite.close();
+            curCSV.close();
+        }
+        catch(Exception sqlEx)
+        {
+            Log.e("MainActivity", sqlEx.getMessage(), sqlEx);
+        }
 
     }
 
